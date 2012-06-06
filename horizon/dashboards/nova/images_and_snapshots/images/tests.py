df0f63a47a9713b0413dd5a83caf401614875371
John Postlethwait <john.postlethwait@nebula.com>
2012-06-06 11:33:22 -0700

    Broken tests
    Change-Id: I36755e873226e27281d066afa64ccd05711861f7

--- horizon/dashboards/nova/images_and_snapshots/images/tests.py
+++ horizon/dashboards/nova/images_and_snapshots/images/tests.py
@@ -24,16 +24,47 @@ from django.core.urlresolvers import reverse
 from horizon import api
 from horizon import test
 
-from mox import IsA
+from mox import IgnoreArg, IsA
 
 
 IMAGES_INDEX_URL = reverse('horizon:nova:images_and_snapshots:index')
 
 
 class ImageViewTests(test.TestCase):
+    def test_image_create_get(self):
+        url = reverse('horizon:nova:images_and_snapshots:images:create')
+        res = self.client.get(url)
+        self.assertTemplateUsed(res,
+                               'nova/images_and_snapshots/images/create.html')
+
+    @test.create_stubs({api.glance: ('image_create',)})
+    def test_image_create_post(self):
+        form_data = {
+                'name': 'Ubuntu 11.10',
+                'copy_from': 'http://cloud-images.ubuntu.com/releases/'
+                            'oneiric/release/ubuntu-11.10-server-cloudimg'
+                            '-amd64-disk1.img',
+                'disk_format': 'qcow2',
+                'minimum_disk': '15',
+                'minimum_ram': '512',
+                'is_public': '1',
+                'method': 'CreateImageForm'
+                }
+
+        api.glance.image_create(IsA(http.HttpRequest), **form_data). \
+                AndReturn(self.images.first())
+        self.mox.ReplayAll()
+
+        url = reverse('horizon:nova:images_and_snapshots:images:create')
+        res = self.client.post(url, form_data)
+
+        self.assertNoFormErrors(res)
+        self.assertEqual(res.status_code, 302)
+
+    @test.create_stubs({api.glance: ('image_get',)})
     def test_image_detail_get(self):
         image = self.images.first()
-        self.mox.StubOutWithMock(api.glance, 'image_get')
+
         api.glance.image_get(IsA(http.HttpRequest), str(image.id)) \
                                  .AndReturn(self.images.first())
         self.mox.ReplayAll()
@@ -45,9 +76,10 @@ class ImageViewTests(test.TestCase):
                                 'nova/images_and_snapshots/images/detail.html')
         self.assertEqual(res.context['image'].name, image.name)
 
+    @test.create_stubs({api.glance: ('image_get',)})
     def test_image_detail_get_with_exception(self):
         image = self.images.first()
-        self.mox.StubOutWithMock(api.glance, 'image_get')
+
         api.glance.image_get(IsA(http.HttpRequest), str(image.id)) \
                   .AndRaise(self.exceptions.glance)
         self.mox.ReplayAll()
